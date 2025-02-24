9a39cdc1133b64c3dc81711cfb4de6a31c218a12
k-pundareekaksh <126395765+k-pundareekaksh@users.noreply.github.com>
2025-02-24 11:11:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install Chrome
 apt-get update && apt-get install -y google-chrome-stable
-
+google-chrome --version
 # Run the app
 streamlit run app.py --server.port 8501 --server.address 0.0.0.0
